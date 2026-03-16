b7cb0212a1f57d7c5a5a8e447ca4867e9e6b5d7c
Dilsara Thiranjaya <dilsarathiranjaya3@gmail.com>
2026-03-16 16:34:04 +0530

    feat: add complete CRUD - download and delete endpoints

--- src/main/java/lk/ijse/eca/fileservice/FileServiceApplication.java
+++ src/main/java/lk/ijse/eca/fileservice/FileServiceApplication.java
@@ -1,13 +1,34 @@
 package lk.ijse.eca.fileservice;
 
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class FileServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileServiceApplication.class, args);
+    }
+
+    @Bean
+    public Storage storage(@Value("${gcp.project-id}") String projectId, @Value("${gcp.credentials-location}") ClassPathResource credentialResource) throws IOException {
+        ServiceAccountCredentials credentials =
+                ServiceAccountCredentials.fromStream(credentialResource.getInputStream());
+
+        return StorageOptions
+                .newBuilder()
+                .setProjectId(projectId)
+                .setCredentials(credentials)
+                .build()
+                .getService();
     }
 }
